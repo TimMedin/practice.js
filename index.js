e93a74811821f49f77389e50600ec894d332954d
TimMedin <timmedin180@gmail.com>
2023-11-11 17:19:23 +0200

    practice js

---
+++ index.js
@@ -0,0 +1,46 @@
+let age = "14";
+console.log(age);
+// вік
+
+const name = "Tim";
+console.log(name); 
+// ім'я
+
+var isStudent = true || false;
+console.log(isStudent = true);
+// IsStudent
+
+const MyString = "Немає нічого неможливого";
+console.log(MyString);
+// цитата
+
+let MyNumber = "20"
+console.log(MyNumber + 10);
+
+const MyNull = "null"
+console.log(MyNull);
+// значення null
+
+
+const promptt = prompt("Введіть ваше ім'я")
+alert(promptt + ", вітаємо на сайті!")
+
+let session = confirm("Ви підтверджуєте дію?")
+if (session) {
+    alert("Дякую за підтвердження!");
+}
+else {
+    alert("Дію відмінено.")
+}
+
+alert("Дія небезпечна!")
+let confirmed = confirm("Підтвердіть небезпечну дію");
+if (confirmed) {
+    alert("Дякую за підтвердження!");
+}
+else {
+    alert("Дію відмінено.")
+}
+
+
+
